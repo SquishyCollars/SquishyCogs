b60250a3b1fb8b69978d972950e625b37205af6e
Squishy <alittlequashera@gmail.com>
2023-06-03 02:42:27 +0200

    3.5

--- roleplay/__init__.py
+++ roleplay/__init__.py
@@ -1,5 +1,5 @@
 from .roleplay import Roleplay
 
-    """Add the cog to the bot."""
-async def setup(bot):
-    await bot.add_cog(Roleplay(bot))
+def setup(bot):
+    n = Roleplay()
+    await bot.add_cog(n)
